108db9355f1a9d5c9e18f638cd809982857e6f7e
Hamid Farmand <135585544+hamid-farmand@users.noreply.github.com>
2025-11-25 11:45:08 +0330

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", 0))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "KashanMarketBot")
